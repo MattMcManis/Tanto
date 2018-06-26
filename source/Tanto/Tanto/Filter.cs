ed658792de2add2b7cffa7bad2628a102846572b
MattMcManis <mattmcmanis@outlook.com>
2018-06-26 02:25:15 -0700

    v0.5.9.3-alpha
    Updated Filter "Remove Episode Numbering"

--- source/Tanto/Tanto/Filter.cs
+++ source/Tanto/Tanto/Filter.cs
@@ -58,7 +58,6 @@ namespace Tanto
                                         MessageBoxButton.OK,
                                         MessageBoxImage.Error);
                     }
-
                 }
             }
 
