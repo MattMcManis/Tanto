1e2b47c60ae6d40b719f523941ecff8a067bb4f5
Matt McManis <mattmcmanis@outlook.com>
2020-12-12 12:22:44 -0800

    Update AssemblyInfo.cs

--- source/Tanto/Tanto/Properties/AssemblyInfo.cs
+++ source/Tanto/Tanto/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tanto")]
-[assembly: AssemblyCopyright("Copyright © 2019 Matt McManis")]
+[assembly: AssemblyCopyright("Copyright © 2020 Matt McManis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.4.0")]
-[assembly: AssemblyFileVersion("0.6.4.0")]
+[assembly: AssemblyVersion("0.6.5.0")]
+[assembly: AssemblyFileVersion("0.6.5.0")]
